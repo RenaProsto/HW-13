b5fbde46887654a8fe869f6b777266fb7dd96a06
RenaProsto <renaprosto123@gmail.com>
2023-04-18 23:35:25 +0500

    Коммит копия Дз 12 с учетом добавления модификатора доступа в классы.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author andrzejSapkowski = new Author("Andrzej" , "Sapkowski");
+        Author joanneRowling = new Author("Joanne","Rowling");
+
+        Book theWitcher = new Book("The Witcher" ,andrzejSapkowski, 1986);
+        Book harryPotter = new Book("Harry Potter and the Philosopher's Stone",joanneRowling, 1997);
+
+        System.out.println(theWitcher.getNameBook() + " " + theWitcher.getYearBook()+ " " + andrzejSapkowski.getNameAuthor()+ " " +andrzejSapkowski.getSurnameAuthor());
+        System.out.println(harryPotter.getNameBook()+ " " +harryPotter.getYearBook()+ " " + joanneRowling.getNameAuthor() + " " +joanneRowling.getSurnameAuthor());
+
+        theWitcher.setYearBook(1985);
+        System.out.println(theWitcher.getYearBook() + " Изменен год.");
+
     }
+
 }
